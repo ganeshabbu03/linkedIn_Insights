fc834824de07dbc6f2983491d24104272aa8a336
ganeshabbu03 <ganeshabbu03@gmail.com>
2025-12-14 15:54:12 +0530

    Prepare for deployment: Update deps and dynamic port

--- run.py
+++ run.py
@@ -7,4 +7,6 @@ if __name__ == "__main__":
     if sys.platform == 'win32':
         asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
